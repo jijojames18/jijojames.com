49e3dc204ebd2f04868d74c833fe1aa92911b737
Jijo James <jijojames18@gmail.com>
2023-09-02 10:10:32 +0530

    Contact form setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
+import { createHead } from '@unhead/vue';
+import { VueRecaptchaPlugin } from 'vue-recaptcha';
+
 import './index.css';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.use(createHead());
+app.use(VueRecaptchaPlugin, {
+  v2SiteKey: import.meta.env.VITE_RECAPTCHA_KEY,
+});
+
+app.mount('#app');
