062e84a9ff8e9291f1b449a2612f226cfa27fb5c
Jijo James <jijojames18@gmail.com>
2023-09-03 12:29:01 +0530

    Work experience

--- src/components/workExperience.vue
+++ src/components/workExperience.vue
@@ -1,5 +1,11 @@
 <script lang="ts" setup></script>
 
 <template>
-  <section></section>
+  <section class="mt-[8%] mb-[4em] px-[5%] min-h-screen">
+    <div class="flex flex-col justify-center flex-row max-w-[80em] mx-[auto] mt-[1em] mb-0 sm:flex-col md:flex-col">
+      <div class="text-white text-[3em] mb-[0.25em] text-left">
+        <h2>Work Experience</h2>
+      </div>
+    </div>
+  </section>
 </template>
