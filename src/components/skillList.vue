a455c14c2e2f35d45ffd26464659f94a1524d1a2
Jijo James <jijojames18@gmail.com>
2023-09-03 10:43:40 +0530

    Skills page

--- src/components/skillList.vue
+++ src/components/skillList.vue
@@ -1,5 +1,40 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const jsIconUrl = new URL('/assets/icon-js.svg', import.meta.url).href;
+const vueIconUrl = new URL('/assets/icon-vue.svg', import.meta.url).href;
+const javaIconUrl = new URL('/assets/icon-java.svg', import.meta.url).href;
+const dockerIconUrl = new URL('/assets/icon-docker.svg', import.meta.url).href;
+const k8sIconUrl = new URL('/assets/icon-k8s.svg', import.meta.url).href;
+const htmlIconUrl = new URL('/assets/icon-html.svg', import.meta.url).href;
+const cssIconUrl = new URL('/assets/icon-css.svg', import.meta.url).href;
+const redisIconUrl = new URL('/assets/icon-redis.svg', import.meta.url).href;
+const tsIconUrl = new URL('/assets/icon-ts.svg', import.meta.url).href;
+const mySQLIconUrl = new URL('/assets/icon-mysql.svg', import.meta.url).href;
+
+const icons = [
+  { icon: jsIconUrl, alt: 'Technology: Javascript' },
+  { icon: vueIconUrl, alt: 'Technology: VueJS' },
+  { icon: htmlIconUrl, alt: 'Technology: HTMl5' },
+  { icon: cssIconUrl, alt: 'Technology: CSS3' },
+  { icon: tsIconUrl, alt: 'Technology: TypeScript' },
+  { icon: javaIconUrl, alt: 'Technology: Java' },
+  { icon: mySQLIconUrl, alt: 'Technology: MySQL' },
+  { icon: redisIconUrl, alt: 'Technology: Redis' },
+  { icon: dockerIconUrl, alt: 'Technology: Docker' },
+  { icon: k8sIconUrl, alt: 'Technology: Kubernetes' },
+];
+</script>
 
 <template>
-  <section></section>
+  <section class="mt-[2em] mb-[4em] px-[5%]">
+    <div class="flex flex-col justify-center flex-row max-w-[80em] mx-[auto] my-0 sm:flex-col md:flex-col">
+      <div class="text-white text-[3em] mb-[0.25em] text-center">
+        <h2>Technology stack</h2>
+      </div>
+      <div class="flex w-full flex-wrap justify-center">
+        <div class="m-[1em]" v-for="({ icon, alt }, index) in icons" :key="index">
+          <img :src="icon" :alt="alt" />
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
