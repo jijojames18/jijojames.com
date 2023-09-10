9d04d87a5d47184c8e2cfc0b0a1f04441b262aaf
Jijo James <jijojames18@gmail.com>
2023-09-10 20:31:58 +0530

    Open source section

--- src/components/openSource.vue
+++ src/components/openSource.vue
@@ -1,5 +1,42 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useFirestore } from 'vuefire';
+import { useCollection } from 'vuefire';
+import { collection } from 'firebase/firestore';
+
+const db = useFirestore();
+
+const projects = useCollection(collection(db, 'projects'));
+</script>
 
 <template>
-  <section></section>
+  <section class="mt-[2em] mb-[4em] px-[5%]" id="openSource">
+    <div class="flex flex-col justify-center max-w-[80em] mx-[auto] mt-[1em] mb-0">
+      <div class="text-white text-[3em] mb-[0.25em] text-center">
+        <h2>Open source projects</h2>
+      </div>
+    </div>
+    <div class="flex flex-wrap">
+      <div role="list" v-for="({ title, image, desc, technology }, i) in projects" :key="i">
+        <div role="listitem">
+          <div>
+            <div>
+              <img :src="image" loading="lazy" :alt="title" />
+            </div>
+            <div>
+              <h3>{{ title }}</h3>
+              <span>
+                {{ desc }}
+              </span>
+              <div v-for="(item, j) in technology" :key="j">
+                <div>{{ item }}</div>
+              </div>
+              <div>
+                <div>View Project</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,20 @@ import App from './App.vue';
 
 import { createHead } from '@unhead/vue';
 import { VueRecaptchaPlugin } from 'vue-recaptcha';
+import { VueFire, VueFireAuth } from 'vuefire';
+import { initializeApp } from 'firebase/app';
 
 import './index.css';
 
+const firebaseApp = initializeApp(JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG));
 const app = createApp(App);
 
 app.use(createHead());
 app.use(VueRecaptchaPlugin, {
   v2SiteKey: import.meta.env.VITE_RECAPTCHA_KEY,
 });
-
+app.use(VueFire, {
+  firebaseApp,
+  modules: [VueFireAuth()],
+});
 app.mount('#app');
